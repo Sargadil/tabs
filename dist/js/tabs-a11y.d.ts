2f0932fb9af836f0509954d885ff5f24159850bf
Bartek Trajder <bartek.trajder@gmail.com>
2026-08-25 12:29:56 +0200

    Add TypeScript declarations for the Tabs class and config

---
+++ dist/js/tabs-a11y.d.ts
@@ -0,0 +1,30 @@
+export interface TabsClasses {
+    tabsNavContainer?: string;
+    tabsNavList?: string;
+    tabsNavButton?: string;
+    tabPanel?: string;
+    tabPanelTitle?: string;
+}
+
+export interface TabsSelectors {
+    tabPanelIdPrefix?: string;
+    tabPanelOpen?: string;
+}
+
+export interface TabsOptions {
+    useCustomNav?: boolean;
+    customNavTitles?: string[];
+    initSelectedItem?: number;
+    removeTabPanelTitle?: boolean;
+}
+
+export interface TabsConfig {
+    contextID?: string;
+    classes?: TabsClasses;
+    selectors?: TabsSelectors;
+    options?: TabsOptions;
+}
+
+export default class Tabs {
+    constructor(configs?: TabsConfig);
+}
